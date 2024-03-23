bb2b9cbd2ada547ad735dad4b096fefd6734393d
Robert <18058627+RobH0@users.noreply.github.com>
2024-03-23 16:41:31 -0700

    Preview post width now resizes when a different image file is selected for a new post.
    To do:
     - fix issue where height of preview section means that the post preview often overflows of screen.

--- public/js/main.js
+++ public/js/main.js
@@ -13,13 +13,15 @@ function fileChosen(){
             previewPostContainer.style.width = "1000px";
             previewImg.src=this.result;
             let currentImgPreview = document.querySelector('.post-pic-img');
-            previewImg.style.maxHeight = "600px";
+            previewImg.style.maxHeight = "100%";
             let newWidth = currentImgPreview.clientWidth;
             console.log(newWidth);
             let newWidthStr = newWidth.toString();
             console.log(newWidthStr)
             console.log(newWidthStr + "px");
-            previewPostContainer.style.width = "200px";//newWidthStr;
+            // fix issue where preview doesn't resize correctly
+            previewPostContainer.style.width = newWidthStr + "px";
+            previewPostContainer.style.maxWidth = "100%";
         });
         
     } else {
