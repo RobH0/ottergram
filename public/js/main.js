93ac62cd709cf365d7632c522b6f94c8ed1f0371
Robert <18058627+RobH0@users.noreply.github.com>
2024-03-23 16:29:16 -0700

    new-post page now shows a preview of the post when user chooses an image file to post
    To do:
    - need to fix post width resizing
    - implement drag and drop zone for image/gif upload

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,27 @@
 const uploadFile = document.querySelector('.choose-file');
-const previewImg = document.querySelector('.post-pic-img')
+const previewImg = document.querySelector('.post-pic-img');
+const previewPostContainer = document.querySelector('.feed-post-container');
 const fileReader = new FileReader();
 
-
+//fix sizing!!!!!!
 function fileChosen(){
     if (uploadFile.value != ""){
         console.log(uploadFile.value);
-        //previewImg.src = uploadFile.result;
         let file = uploadFile.files[0];
         fileReader.readAsDataURL(file);
         fileReader.addEventListener("load", function (){
+            previewPostContainer.style.width = "1000px";
             previewImg.src=this.result;
-        })
+            let currentImgPreview = document.querySelector('.post-pic-img');
+            previewImg.style.maxHeight = "600px";
+            let newWidth = currentImgPreview.clientWidth;
+            console.log(newWidth);
+            let newWidthStr = newWidth.toString();
+            console.log(newWidthStr)
+            console.log(newWidthStr + "px");
+            previewPostContainer.style.width = "200px";//newWidthStr;
+        });
+        
     } else {
         console.log('no file');
     }
