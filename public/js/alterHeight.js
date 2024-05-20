baf1af6db4f7dcebf30f2c53e47c0d19ebab4328
Robert <18058627+RobH0@users.noreply.github.com>
2024-05-19 19:06:06 -0700

    Customized scrollbar

---
+++ public/js/alterHeight.js
@@ -0,0 +1,8 @@
+const postContainer = document.querySelector('.post-section');
+const commentsSection = document.querySelector('.comments-section');
+const mainPostPage = document.querySelector('.main-post-page');
+
+//console.log(`postContainer.clientHeight: ${postContainer.clientHeight}`)
+//commentsSection.style.height = `${postContainer.clientHeight}px`;
+
+
