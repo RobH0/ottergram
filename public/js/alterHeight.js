3788151a0669da283adbc11651b26dd0992e29b1
Robert <18058627+RobH0@users.noreply.github.com>
2024-05-23 16:25:58 -0700

    Comment section height updates to match post section on post.ejs view when the window is resized.

--- public/js/alterHeight.js
+++ public/js/alterHeight.js
@@ -1,42 +1,26 @@
 let postImg = document.querySelector('.post-pic img');
 
-console.log(postImg);
-
-
-postImg.addEventListener('load', (event) => { matchedHeights()});
-
 let heightChanged = false;
-
-while (heightChanged == false){
-    try{
-        if (postImg.complete){
-            console.log('MatchedHeights called')
-            let postContainer = document.querySelector('.post-section');
-            console.log(postContainer.clientHeight);
-            let commentsSection = document.querySelector('.comments-section');
-            let newHeight = postContainer.clientHeight;
-            newHeight = newHeight + 'px';
-            console.log(`newHeight: ${newHeight}`);
-            commentsSection.style.height = newHeight;
-            heightChanged = true;
+function matchHeights(heightChanged){
+    while (heightChanged == false){
+        try{
+            if (postImg.complete){
+                console.log('MatchedHeights called')
+                let postContainer = document.querySelector('.post-section');
+                console.log(postContainer.clientHeight);
+                let commentsSection = document.querySelector('.comments-section');
+                let newHeight = postContainer.clientHeight;
+                newHeight = newHeight + 'px';
+                console.log(`newHeight: ${newHeight}`);
+                commentsSection.style.height = newHeight;
+                heightChanged = true;
+            }
+        } catch (err){
+            console.error(err);
         }
-    } catch (err){
-        console.error(err);
     }
 }
 
+window.addEventListener('resize', () => { matchHeights(false)});
 
-function matchedHeights() {
-    console.log('MatchedHeights called')
-    let postContainer = document.querySelector('.post-section');
-    let commentsSection = document.querySelector('.comments-section');
-    let newHeight = postContainer.style.height;
-    console.log(`newHeight: ${newHeight}`);
-    commentsSection.style.height = newHeight;
-
-
-}
-
-
-
-
+matchHeights(heightChanged);
