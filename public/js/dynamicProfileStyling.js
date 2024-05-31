11a8e20a05f70d414166ff5deb426a83667df64c
Robert <18058627+RobH0@users.noreply.github.com>
2024-05-31 11:37:28 -0700

    Profile page styling made responsive for mobile and tablet devices.

--- public/js/dynamicProfileStyling.js
+++ public/js/dynamicProfileStyling.js
@@ -3,8 +3,17 @@ const gallery = document.querySelector('.gallery');
 console.log(gallery.children.length);
 
 // Dynamically change gallery's grid column template to prevent overlaping when less than 3 posts exist on a user's profile.
-if (gallery.children.length == 2){
-    gallery.style.gridTemplateColumns = '50% 50%';
-} else if (gallery.children.length == 1){
-    gallery.style.gridTemplateColumns = '100%';
+function handleResize(){
+    console.log('handling resize on profile page.')
+    if (window.innerWidth > 1000){
+        if (gallery.children.length == 2){
+            gallery.style.gridTemplateColumns = '50% 50%';
+        } else if (gallery.children.length == 1){
+            gallery.style.gridTemplateColumns = '100%';
+        }
+    }
 }
+
+window.addEventListener('resize', handleResize);
+
+handleResize();
