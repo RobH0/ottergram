9c5e77f280933eec65dc7a9ff5a251452b4c0f3f
Robert <18058627+RobH0@users.noreply.github.com>
2024-05-31 15:33:52 -0700

    Post page styling is now responsive for mobiles and tablets.

--- public/js/alterHeight.js
+++ public/js/alterHeight.js
@@ -2,23 +2,26 @@ let postImg = document.querySelector('.post-pic img');
 
 let heightChanged = false;
 function matchHeights(heightChanged){
-    while (heightChanged == false){
-        try{
-            if (postImg.complete){
-                console.log('MatchedHeights called')
-                let postContainer = document.querySelector('.post-section');
-                console.log(postContainer.clientHeight);
-                let commentsSection = document.querySelector('.comments-section');
-                let newHeight = postContainer.clientHeight;
-                newHeight = newHeight + 'px';
-                console.log(`newHeight: ${newHeight}`);
-                commentsSection.style.height = newHeight;
-                heightChanged = true;
+    if (window.innerWidth > 931){
+        while (heightChanged == false){
+            try{
+                if (postImg.complete){
+                    console.log('MatchedHeights called')
+                    let postContainer = document.querySelector('.post-section');
+                    console.log(postContainer.clientHeight);
+                    let commentsSection = document.querySelector('.comments-section');
+                    let newHeight = postContainer.clientHeight;
+                    newHeight = newHeight + 'px';
+                    console.log(`newHeight: ${newHeight}`);
+                    commentsSection.style.height = newHeight;
+                    heightChanged = true;
+                }
+            } catch (err){
+                console.error(err);
             }
-        } catch (err){
-            console.error(err);
         }
     }
+    
 }
 
 window.addEventListener('resize', () => { matchHeights(false)});
