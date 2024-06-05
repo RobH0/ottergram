93c1dfd0814c625afcc17bff9a4cee115b0b375e
Robert <18058627+RobH0@users.noreply.github.com>
2024-06-05 12:05:27 -0700

    Improved login/register error message styling.

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -40,24 +40,23 @@ module.exports = {
        
     },
 
-    postRegister: async (req, res) => {
+    postRegister: async (req, res, next) => {
         try{
             let newUsername = req.body.username;
             let newPassword = req.body.password;
-            let invalidCredsMsg = ["Please make sure both Username and Password fields are filled out."]
+            let invalidCredsMsg = ["Please ensure that both the Username and Password fields are filled out."]
             
             if (newUsername == "" || newPassword == ""){
                 req.flash('error', invalidCredsMsg)
-                console.log("Please make sure to fill out both username and password fields.")
                 res.redirect('/register');
             // Ensures minimum password is set to at least 8 characters as recommended by OWASP authentication cheatsheet: https://cheatsheetseries.owasp.org/cheatsheets/Authentication_Cheat_Sheet.html
             } else if (newPassword.length < 8){
-                req.flash('error', "Your password must be at least 8 characters in length");
+                req.flash('error', "Your password must be at least 8 characters in length.");
                 res.redirect('/register');
             
             // Makes sure passwords submitted during registration are less likely to exceed the 72 byte limit allocated for the password string that bcrypt hashes.
             } else if (newPassword.length > 50){
-                req.flash('error', "Your password exceeds the maximum password length of 50 characters");
+                req.flash('error', "The password you entered exceeds the maximum length of 50 characters.");
                 res.redirect('/register');
             }else{
                 const hashedPassword = await bcrypt.hash(newPassword, 10);
