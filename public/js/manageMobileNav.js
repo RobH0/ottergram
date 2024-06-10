30cf2c06f1c4cc28cfed963866bad2d9570a4cdf
Robert <18058627+RobH0@users.noreply.github.com>
2024-06-10 15:44:32 -0700

    Implemented basic nav design for mobile devices on authenticated feed page.

--- public/js/manageMobileNav.js
+++ public/js/manageMobileNav.js
@@ -1,5 +1,6 @@
 const openMobileNavBtn = document.querySelector('.open-mobile-menu-btn');
 const closeMobileNavBtn = document.querySelector('.close-mobile-menu-btn');
+const openedMobileNav = document.querySelector('.opened-mobile-right-nav');
 
 openMobileNavBtn.addEventListener('click', openMobileNav);
 closeMobileNavBtn.addEventListener('click', closeMobileNav);
@@ -9,14 +10,14 @@ function openMobileNav(){
     openMobileNavBtn.classList.add('inactive');
     openMobileNavBtn.classList.remove('active');
 
-    closeMobileNavBtn.classList.add('active');
-    closeMobileNavBtn.classList.remove('inactive');
+    openedMobileNav.classList.add('active');
+    openedMobileNav.classList.remove('inactive');
 }
 
 function closeMobileNav(){
     console.log('Closing mobile nav');
-    closeMobileNavBtn.classList.add('inactive');
-    closeMobileNavBtn.classList.remove('active');
+    openedMobileNav.classList.add('inactive');
+    openedMobileNav.classList.remove('active');
 
     openMobileNavBtn.classList.add('active');
     openMobileNavBtn.classList.remove('inactive');
