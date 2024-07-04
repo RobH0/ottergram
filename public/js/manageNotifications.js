e542d592e824ecc141cf0e662d270dbef2b5f48d
Robert <18058627+RobH0@users.noreply.github.com>
2024-07-03 18:10:06 -0700

    Started implementing notification list for mobile devices.

--- public/js/manageNotifications.js
+++ public/js/manageNotifications.js
@@ -1,18 +1,29 @@
-const notificationBtn = document.querySelector('.notification-btn');
-const notificationsDropDown = document.querySelector('.notifications-drop-down');
+const notificationBtns = document.querySelectorAll('.notification-btn');
+const notificationsDropDown = document.querySelectorAll('.notifications-drop-down');
 
-notificationBtn.addEventListener('click', (event) => {
+/*notificationBtns.addEventListener('click', (event) => {
     manageDropDown(event.target);
-})
+})*/
+
+notificationBtns.forEach((element) => element.addEventListener('click', (event) => { manageDropDown(event.target) }));
 
 function manageDropDown(target){
     console.log('in manageDropDown')
-    console.log(`notificationsDropDown.style.display: ${notificationsDropDown.style.display}`);
-    if (notificationsDropDown.style.display == '' || notificationsDropDown.style.display == 'none'){
+    console.log(`notificationsDropDown ${notificationsDropDown}`);
+    notificationsDropDown.forEach((element) => {
+        if (element.style.display == '' || element.style.display == 'none'){
+            console.log('in if');
+            element.style.display = 'block';   
+        } else if (element.style.display == 'block'){
+            console.log('else if');
+            element.style.display = 'none';
+        }
+    })
+    /*if (notificationsDropDown.style.display == '' || notificationsDropDown.style.display == 'none'){
         console.log('in if');
         notificationsDropDown.style.display = 'block';   
     } else if (notificationsDropDown.style.display == 'block'){
         console.log('else if');
         notificationsDropDown.style.display = 'none';
-    }
+    }*/
 }
