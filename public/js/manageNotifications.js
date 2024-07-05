69c938424f71ce5ac1afc72325f574c77e489b2f
Robert <18058627+RobH0@users.noreply.github.com>
2024-07-05 12:30:24 -0700

    Notifications drop down now closes when a user clicks off to the side of it.

--- public/js/manageNotifications.js
+++ public/js/manageNotifications.js
@@ -7,9 +7,48 @@ const notificationsDropDown = document.querySelectorAll('.notifications-drop-dow
 
 notificationBtns.forEach((element) => element.addEventListener('click', (event) => { manageDropDown(event.target) }));
 
-function manageDropDown(target){
+try{
+    document.addEventListener('click', function(e){
+        let target;
+        let isNotificationDropDown = false;
+        let isBlock = false;
+        notificationsDropDown.forEach((element) => {
+            if (element.contains(e.target)){
+                console.log(e.target);
+                console.log('in if element contains');
+                isNotificationDropDown = true;
+                console.log(`1 ${isNotificationDropDown}`);
+                console.log(`element.style.display ${element.style.display}`);
+                
+            }
+            if (element.style.display == 'block'){
+                isBlock = true;
+            }
+        });
+
+        let isNotificationBtn = false;
+        notificationBtns.forEach((element) => {
+            if (element.contains(e.target)){
+                isNotificationBtn = true;
+            }
+        });
+        
+        console.log(`isNotifiactionDropdown ${isNotificationDropDown}`)
+        console.log(`isBlock ${isBlock}`)
+        if (isNotificationDropDown == false && isBlock && isNotificationBtn == false){
+            console.log(`in second if`);
+            manageDropDown();
+        }  
+    })
+} catch(err){
+    console.error(err);
+}
+
+
+
+function manageDropDown(){
     console.log('in manageDropDown')
-    console.log(`notificationsDropDown ${notificationsDropDown}`);
+    console.log(`notificationsDropDown ${JSON.stringify(notificationsDropDown)}`);
     notificationsDropDown.forEach((element) => {
         if (element.style.display == '' || element.style.display == 'none'){
             console.log('in if');
